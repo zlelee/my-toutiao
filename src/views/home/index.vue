9e1a987bd2594ae16887597bef59a192c385da4f
lele <562282501@qq.com>
2021-02-27 21:17:49 +0800

    基础样式调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -6,7 +6,13 @@
       >
     </van-nav-bar>
     <!-- Tab标签页 -->
-    <van-tabs v-model="active" animated swipeable swipe-threshold="3">
+    <van-tabs
+      class="channel-tabs"
+      v-model="active"
+      animated
+      swipeable
+      swipe-threshold="3"
+    >
       <van-tab title="标签 1">内容 1</van-tab>
       <van-tab title="标签 2">内容 2</van-tab>
       <van-tab title="标签 3">内容 3</van-tab>
@@ -47,6 +53,31 @@ export default {
   }
   /deep/ .van-nav-bar__title {
     max-width: unset;
+  }
+  /deep/ .channel-tabs {
+    .van-tab {
+      border-right: 1px solid #edeff3;
+      min-width: 200px;
+      height: 80px;
+      .van-tab__text {
+        font-size: 27px;
+        color: #777777;
+      }
+    }
+
+    .van-tab--active .van-tab__text {
+      color: #333 !important;
+    }
+    .van-tabs__line {
+      width: 31px !important;
+      height: 6px;
+      background: rgba(50, 150, 250, 1);
+      border-radius: 3px;
+      bottom: 8px;
+    }
+    .van-tabs__nav {
+      padding: 0;
+    }
   }
 }
 </style>
