f744093c74b36fafa7ca3a82eeef3250cb02f7f5
lele <562282501@qq.com>
2021-02-28 19:35:36 +0800

    下拉刷新

--- src/components/article-list.vue
+++ src/components/article-list.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home-container">
+    <van-pull-refresh v-model="refreshing" @refresh="onRefresh" success-duration="1500" :success-text="successText">
     <van-list
       v-model="loading"
       :finished="finished"
@@ -14,6 +15,7 @@
         :title="item.title"
       />
     </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -32,7 +34,10 @@ export default {
       list: [],
       loading: false,
       finished: false,
-      error: false
+      error: false,
+      refreshing: false,
+      timestamp: null,
+      successText: '刷新成功'
     }
   },
   created() {},
@@ -42,24 +47,46 @@ export default {
       try {
         const { data } = await getArticles({
           channel_id: this.channel.id,
-          timestamp: Date.now(),
+          timestamp: this.timestamp || Date.now(),
           with_top: 1
         })
         // 往数组中追加数据
         const { results } = data.data
         this.list.push(...results)
-
         // 模拟错误
-        if (Math.random() > 0.2) {
+        /*  if (Math.random() > 0.2) {
           JSON.parse('ssfafsafsa')
-        }
+        } */
         this.loading = false
-        if (!results.length) {
+        // 判断数据是否加载结束
+        if (results.length) {
+          // 更新获取下一页数据的时间戳
+          this.timestamp = results.pre_timestamp
+        } else {
+          // 没有数据了，设置加载状态结束，不再触发上拉加载更多了
           this.finished = true
         }
       } catch (err) {
         this.error = true
         this.loading = false
+      }
+    },
+    // 下拉刷新
+    async onRefresh() {
+      try {
+        const { data } = await getArticles({
+          channel_id: this.channel.id,
+          timestamp: Date.now(),
+          with_top: 1
+        })
+        // 往数组中追加数据
+        const { results } = data.data
+        this.list.unshift(...results)
+        this.refreshing = false
+        this.successText = `刷新成功,共更新了${results.length}条数据`
+      } catch (err) {
+        this.refreshing = false
+        this.successText = '刷新失败,请稍后再试'
       }
     }
   }
