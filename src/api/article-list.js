17af2616da0c1b1bd7e81ad5347496ab2a69db4f
lele <562282501@qq.com>
2021-02-28 18:42:14 +0800

    请求频道数据

--- src/api/article-list.js
+++ src/api/article-list.js
@@ -5,3 +5,11 @@ export const getArticleList = () => {
     url: '/app/v1_0/user/channels'
   })
 }
+
+export const getArticles = params => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_1/articles',
+    params
+  })
+}
--- src/components/article-list.vue
+++ src/components/article-list.vue
@@ -6,12 +6,13 @@
       finished-text="没有更多了"
       @load="onLoad"
     >
-      <van-cell v-for="item in list" :key="item" :title="item" />
+      <van-cell v-for="(item,index) in list" :key="index" :title="item.title" />
     </van-list>
   </div>
 </template>
 
 <script>
+import { getArticles } from '@/api/article-list'
 export default {
   name: '',
   props: {
@@ -27,24 +28,21 @@ export default {
       finished: false
     }
   },
-
+  created() {
+  },
   methods: {
-    onLoad() {
+    async onLoad() {
       // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-
-        // 加载状态结束
-        this.loading = false
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 1000)
+      try {
+        const { data } = await getArticles({
+          channel_id: this.channel.id,
+          timestamp: Date.now(),
+          with_top: 1
+        })
+        this.list = data.data.results
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 }
