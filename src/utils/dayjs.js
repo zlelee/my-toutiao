daca3696aa199ab9b46e8a5829cf1fbd92d9a56c
lele <562282501@qq.com>
2021-03-01 14:25:04 +0800

    处理相对时间

--- src/utils/dayjs.js
+++ src/utils/dayjs.js
@@ -1,8 +1,16 @@
 // 加载 dayjs 的包
 import dayjs from 'dayjs'
+import Vue from 'vue'
+import 'dayjs/locale/zh-cn'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+// 配置使用处理相对时间的插件
+dayjs.extend(relativeTime)
 
 // 加载 dayjs 的中文包
 dayjs.locale('zh-cn')
-
 // 使用
-console.log(dayjs().format('YYYY-MM-DD'))
+// 全局过滤器：处理相对时间
+Vue.filter('relativeTime', value => {
+  return dayjs().to(dayjs(value))
+})
