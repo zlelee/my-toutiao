2fcd0c8db6b5b27e8a14ad4d7820a7a1d749a460
lele <562282501@qq.com>
2021-03-01 14:49:18 +0800

    样式调整

--- src/views/home/edit-channel/index.vue
+++ src/views/home/edit-channel/index.vue
@@ -1,27 +1,37 @@
 <template>
   <div class="channel-edit">
-    <!-- 我的频道 -->
-    <van-cell :border="false" title="我的频道">
-      <van-button size="mini" round type="danger" plain>编辑</van-button>
+    <!-- 我的频道标题 -->
+    <van-cell :border="false">
+      <div slot="title" class="title-text">我的频道</div>
+      <van-button class="edit-btn" type="danger" plain round size="mini"
+        >编辑</van-button
+      >
     </van-cell>
-    <!-- 宫格 -->
-    <van-grid :column-num="4" :gutter="10">
+    <!-- 我的频道内容 -->
+    <van-grid class="my-grid" :gutter="10">
       <van-grid-item
-        v-for="value in 8"
-        :key="value"
+        class="grid-item"
+        v-for="(value, index) in 8"
+        :key="index"
         text="文字"
-      />
+        icon="clear"
+      >
+      </van-grid-item>
     </van-grid>
-    <!-- 频道推荐 -->
-      <van-cell :border="false" title="频道推荐">
+    <!-- 频道推荐标题 -->
+    <van-cell :border="false">
+      <div slot="title" class="title-text">频道推荐</div>
     </van-cell>
-    <!-- 宫格 -->
-    <van-grid :column-num="4" :gutter="10">
+    <!-- 频道推荐内容 -->
+    <van-grid class="recommend-grid" :gutter="10">
       <van-grid-item
-        v-for="value in 8"
-        :key="value"
+        class="grid-item"
+        v-for="(value, index) in 8"
+        :key="index"
+        icon="plus"
         text="文字"
-      />
+      >
+      </van-grid-item>
     </van-grid>
   </div>
 </template>
@@ -43,4 +53,63 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.channel-edit {
+  padding: 85px 0;
+  .title-text {
+    font-size: 32px;
+    color: #333333;
+  }
+  // 编辑按钮
+  .edit-btn {
+    width: 104px;
+    height: 48px;
+    font-size: 26px;
+    color: #f85959;
+    border: 1px solid #f85959;
+  }
+  /deep/ .grid-item {
+    width: 160px;
+    height: 86px;
+    .van-grid-item__content {
+      white-space: nowrap;
+      background-color: #f4f5f6;
+      .van-grid-item__text,
+      .text {
+        font-size: 28px;
+        color: #222;
+        margin-top: 0;
+      }
+      .active {
+        color: red;
+      }
+      .van-grid-item__icon-wrapper {
+        position: unset;
+      }
+    }
+  }
+  /deep/ .my-grid {
+    .grid-item {
+      .van-icon-clear {
+        position: absolute;
+        right: -10px;
+        top: -10px;
+        font-size: 30px;
+        color: #cacaca;
+        z-index: 2;
+      }
+    }
+  }
+  /deep/ .recommend-grid {
+    .grid-item {
+      .van-grid-item__content {
+        flex-direction: row;
+        .van-icon-plus {
+          font-size: 28px;
+          margin-right: 6px;
+        }
+      }
+    }
+  }
+}
+</style>
