3ba27ddd85ebdcb312c87b8d4d0444a20e75a54e
lele <562282501@qq.com>
2021-03-01 14:43:18 +0800

    页面布局

--- src/views/home/edit-channel/index.vue
+++ src/views/home/edit-channel/index.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
-    {{myChannels}}
+  <div class="channel-edit">
+    <!-- 我的频道 -->
+    <van-cell :border="false" title="我的频道">
+      <van-button size="mini" round type="danger" plain>编辑</van-button>
+    </van-cell>
+    <!-- 宫格 -->
+    <van-grid :column-num="4" :gutter="10">
+      <van-grid-item
+        v-for="value in 8"
+        :key="value"
+        text="文字"
+      />
+    </van-grid>
+    <!-- 频道推荐 -->
+      <van-cell :border="false" title="频道推荐">
+    </van-cell>
+    <!-- 宫格 -->
+    <van-grid :column-num="4" :gutter="10">
+      <van-grid-item
+        v-for="value in 8"
+        :key="value"
+        text="文字"
+      />
+    </van-grid>
   </div>
 </template>
 
@@ -13,15 +35,12 @@ export default {
       required: true
     }
   },
-  data () {
-    return {
-    }
+  data() {
+    return {}
   },
 
   methods: {}
 }
 </script>
 
-<style lang='less' scoped>
-
-</style>
+<style lang="less" scoped></style>
