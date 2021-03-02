2191f461e9b05211e23f0c9c157d24a9e0609444
lele <562282501@qq.com>
2021-03-02 20:57:02 +0800

    点击取消返回

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    search
+  <div class="search-container">
+    <form action="/">
+      <van-search
+        v-model="searchText"
+        show-action
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="onCancel"
+        background="#3296fa"
+      />
+    </form>
   </div>
 </template>
 
@@ -8,15 +17,30 @@
 export default {
   name: 'Search',
 
-  data () {
+  data() {
     return {
+      searchText: ''
     }
   },
 
-  methods: {}
+  methods: {
+    onSearch(val) {
+      console.log(val)
+    },
+    onCancel() {
+      this.$router.back()
+    }
+  }
 }
 </script>
 
-<style lang='less' scoped>
-
+<style lang="less" scoped>
+.search-container{
+  .van-search__action{
+    color: #fff;
+  }
+  .van-search__action:active{
+    background: transparent;
+  }
+}
 </style>
